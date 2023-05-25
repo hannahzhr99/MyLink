905b873bda7c107aafc7157f95dc0daedb8bd2ea
R <rajifafif@gmail.com>
2023-05-25 11:24:44 +0700

    Clear Up

---
+++ MyLink/ShowMapView.swift
@@ -0,0 +1,152 @@
+//
+//  ShowMapView.swift
+//  MyLink
+//
+//  Created by Rajif Afif on 25/05/23.
+//
+
+import CoreLocationUI
+import MapKit
+import SwiftUI
+
+struct ShowMapView: View {
+    @State private var mapUserTrackingMode: MapUserTrackingMode = .follow
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: -6.301275723658521,
+                                       longitude: 106.65313628889636),
+        latitudinalMeters: 2000,
+        longitudinalMeters: 2000
+    )
+    @StateObject private var viewModel = LocationViewModel()
+    
+    @State var destination: String = ""
+    @State var searchresultopen: Bool = false
+    @State private var annotations = [
+        HalteBSDLink(coordinate: CLLocationCoordinate2D(latitude: -6.301275723658521, longitude: 106.65313628889636), title: "The Breeze"),
+        HalteBSDLink(coordinate: CLLocationCoordinate2D(latitude: -6.3021288399689785, longitude: 106.65124588037023), title: "Sinar Mas Land"),
+        HalteBSDLink(coordinate: CLLocationCoordinate2D(latitude: -6.32084247949462, longitude: 106.64364252576979), title: "Terminal Intermoda"),
+        HalteBSDLink(coordinate: CLLocationCoordinate2D(latitude: -6.303551348834457, longitude: 106.65013956132293), title: "CBD Timur")
+    ]
+    
+    var searchResults: [Location] {
+        if destination.isEmpty {
+            return locations
+        } else {
+            return locations.filter { $0.destinationPoint.contains(destination) }
+        }
+    }
+    
+    var body: some View {
+        
+        ZStack () {
+            Map(
+                coordinateRegion: $region,
+                showsUserLocation: true,
+                userTrackingMode: $mapUserTrackingMode,
+                annotationItems: annotations
+            ) { annotation in
+                MapAnnotation(coordinate: annotation.coordinate) {
+                    VStack {
+                        Text(annotation.title)
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                        Image(systemName: "mappin.circle.fill")
+                            .foregroundColor(.black)
+                            .font(.title)
+                        }
+                    }
+                }
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack (alignment: .trailing) {
+                //location button
+                LocationButton(.currentLocation) {
+                    if let currentLocation = viewModel.currentLocation {
+                        region = MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude),
+                            latitudinalMeters: 750,
+                            longitudinalMeters: 750
+                        )
+                    }
+                }
+                .foregroundColor(.white)
+                .cornerRadius(6)
+                .labelStyle(.iconOnly)
+                .symbolVariant(.fill)
+                .tint(Color("PO-500"))
+                .padding(.bottom, 50)
+                
+                Spacer()
+                
+                //searchbar
+                HStack(spacing: 20) {
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .padding(.leading)
+                        .foregroundColor(Color("PO-500"))
+                    TextField("Where do you want to go?", text: $destination)
+                        .accentColor(Color(UIColor.systemGray))
+                        .onChange(of: destination) {newValue in
+                            searchresultopen = (newValue != "")
+                        }
+                    
+                }
+                .frame(width: 358, height: 50).background(RoundedRectangle(cornerRadius: 10).foregroundColor(.white))
+                
+                if searchresultopen {
+                    // list searchbar
+                    ScrollView {
+                        VStack(alignment: .leading, spacing: 0) {
+                            // list of filtered
+                            ForEach(searchResults) { place in
+                                VStack(alignment: .leading, spacing: -0) {
+                                    VStack(alignment: .leading, spacing: 1) {
+                                        HStack {
+                                            Text(place.namaRute)
+                                                .font(.system(size: 14))
+                                            //                                        Text("\(place.place.locality ?? ""), \(place.place.country ?? "")")
+                                            //                                            .opacity(0.7)
+                                            //                                            .font(.system(size: 12))
+                                            Spacer()
+                                            Image(systemName: "chevron.right")
+                                                .padding(.horizontal)
+                                                .font(.system(size: 14))
+                                                .foregroundColor(Color(UIColor.systemGray))
+                                        }
+                                        .padding(.leading, 16)
+                                        .padding(.vertical, 16)
+                                        .foregroundColor(.black)
+                                        .onTapGesture {
+                                        }
+                                        .background(RoundedRectangle(cornerRadius: 6)
+                                            .foregroundColor(.white))
+                                        Spacer()
+                                            .frame(height: 0)
+                                    }
+                                    .frame(alignment: .leading)
+                                    .transition(.opacity)
+                                }
+                            }
+                            .frame(width: 358)
+
+                        }
+                        //                        .frame(alignment: .leading)
+                    }
+                    .frame(height: 200)
+                    Spacer()
+                }
+            }
+        }
+        .onAppear {
+            viewModel.requestAuthorization()
+        }
+    }
+}
+
+struct ShowMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowMapView()
+    }
+}
